18d0aeb66e975b7a943b8bf8bb43b8640c75016e
Carlos Santos <carlos.santos@i3television.es>
2017-03-04 20:56:15 +0100

    Add files via upload

--- carlos/cajero.js
+++ carlos/cajero.js
@@ -1,27 +1,54 @@
 
-var cuentas={
-	"abcd1234":1008,
-	"dfde5555":1234,
-	"cafe4567":12023,
-};
+
+var cuentas={};
 
 var cajero={
 
 entidad:"BBVA",
 id:"1234",
 moneda:"euros",
-saldo:1000,
-ingresoCliente:function(dinero,numCuenta){
-						cuentas[numCuenta]=cuentas[numCuenta]+dinero;
-						cajero.saldo=cajero.saldo+dinero;
+keyAdmin:"cafebabe1234",
+saldo:0,
+ingresoCajero:function(dinero,keyAdmin){
+						if(cajero.keyAdmin==keyAdmin) {
+							cajero.saldo=cajero.saldo+dinero;
+							console.log("Ingreso en cajero de "+dinero+" euros. Saldo en cajero: "+cajero.saldo+" euros");
+						}
+						else console.log("Clave de administrador no valida");
 					},
-ingresoCajero:function(dinero){
-						cajero.saldo=cajero.saldo+dinero;
+
+crearCliente:function(numCuenta,key,keyAdmin){
+						if(keyAdmin===cajero.keyAdmin) {
+							cuentas[numCuenta]={saldo:0,key:key};
+							console.log("Cuenta "+numCuenta+" creada");
+						}
+						else console.log("Clave de administrador no valida");
 					},
-retiradaCliente:function(dinero,numCuenta){
-						cuentas[numCuenta]=cuentas[numCuenta]-dinero;
-						cajero.saldo=cajero.saldo-dinero;
-					}
+
+borrarCliente:function(numCuenta,keyAdmin){
+						if(keyAdmin===cajero.keyAdmin) {
+							delete cuentas[numCuenta];
+							console.log("Cuenta "+numCuenta+" eliminada");
+						}
+						else console.log("Clave de administrador no valida");
+					},
+
+
+ingresoCliente:function(dinero,numCuenta,key){
+						if(cuentas[numCuenta].key===key){
+							cuentas[numCuenta].saldo=cuentas[numCuenta].saldo+dinero;
+							cajero.saldo=cajero.saldo+dinero;
+							console.log("Ingreso de efectivo en cuenta "+numCuenta+" de "+dinero+" euros. Saldo en cuenta: "+cuentas[numCuenta].saldo);
+						} else console.log("Clave de usuario no valida");
+					},
+
+retiradaCliente:function(dinero,numCuenta,key){
+						if(cuentas[numCuenta].key===key){
+							cuentas[numCuenta].saldo=cuentas[numCuenta].saldo-dinero;
+							cajero.saldo=cajero.saldo-dinero;
+							console.log("Retirada de efectivo en cuenta "+numCuenta+" de "+dinero+" euros. Saldo en cuenta: "+cuentas[numCuenta].saldo);
+						} else console.log("Clave de usuario no valida");
+					},
 
 
 
@@ -29,10 +56,28 @@ retiradaCliente:function(dinero,numCuenta){
 
 }
 
-console.log(cajero.ingresoCliente(50,"abcd1234"));
-cajero.retiradaCliente(150,"cafe4567");
+cajero.ingresoCajero(2000,"cafebabe1234");
 
 
-console.log("Saldo Cajero: "+cajero.saldo);
-console.log("Saldo Cuenta abcd1234: "+cuentas.abcd1234);
-console.log("Saldo Cajero cafe4567: "+cuentas.cafe4567);
+cajero.crearCliente("qwerty1234","ca12","cafebabe1234");
+cajero.crearCliente("abcde4567","ab45","cafebabe1234");
+cajero.crearCliente("bacaf7788","vfgh","cafebabe1234");
+cajero.crearCliente("asdfg6543","iopl","cafebabe1234");
+
+
+cajero.ingresoCliente(5000,"qwerty1234","ca12");
+cajero.ingresoCliente(300,"abcde4567","ab45");
+cajero.ingresoCliente(8900,"bacaf7788","vfgh");
+cajero.ingresoCliente(6500,"asdfg6543","iopl");
+
+cajero.retiradaCliente(900,"bacaf7788","vfgh");
+cajero.retiradaCliente(150,"qwerty1234","ca12");
+
+console.log(cuentas);
+
+console.log(cajero);
+
+cajero.borrarCliente("abcde4567","cafebabe1234");
+console.log(cuentas);
+
+
